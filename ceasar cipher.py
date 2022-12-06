8166307b8ac33c949cf47ffb332ec41a351b07dc
oneofthecodersofalltime <112718556+oneofthecodersofalltime@users.noreply.github.com>
2022-12-06 17:41:04 +0200

    ceasar cipher 2
    improved the code

--- ceasar cipher.py
+++ ceasar cipher.py
@@ -6,11 +6,14 @@ def research_mutator(user_input, shift):
     input_list[:0] = user_input
     mutated_list = []
     il_size = len(input_list)
-    while il_size != 0:
+    while il_size:
         il_size -= 1
         new_int = ord(input_list[il_size]) + int(shift)
         new_char = chr(new_int)
-        mutated_list.insert(0, new_char)
+        if input_list[il_size] != ' ':
+            mutated_list.insert(0, new_char)
+        else:
+            mutated_list.insert(0, ' ')
     changed_string = "".join(str(e) for e in mutated_list)
     return changed_string
 
